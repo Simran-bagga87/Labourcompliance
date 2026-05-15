fc1b497ec8cab365e8f2680fb486528010044f3e
Simran-bagga87 <167749495+Simran-bagga87@users.noreply.github.com>
2026-05-15 10:47:03 +0530

    Update 1_Registration.py

--- pages/1_Registration.py
+++ pages/1_Registration.py
@@ -4,8 +4,9 @@ from supabase import create_client
 # ----------------------------
 # SUPABASE
 # ----------------------------
-url = "https://rlkuybqydwrzrcyyosjp.supabase.co"
-key = "sb_publishable_rjAdZJ8AU9FHSVWRR4bvmQ_RctfF2KD"
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
+
 supabase = create_client(url, key)
 
 
